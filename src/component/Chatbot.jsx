02559e7ac6ef65bcc2b17a62b4d20bf389aac843
purnale <bhaktipurnale89@gmail.com>
2025-03-11 14:51:02 +0530

    add

--- src/component/Chatbot.jsx
+++ src/component/Chatbot.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Chatbot = () => {
-    // AIzaSyAnSHosGWzqF9jxschkOhXz4RK-o0grJMk
+  const [inputData, setInputData] = useState("");
+
+  const sendingData = {
+    contents: [
+      {
+        parts: [{ text: ""}],
+      },
+    ],
+  };
+
+  // AIzaSyAKw1ubVv54QZ4hARyQLItJ73tGE_EnCTo
+
+  const handleInput = (event) => {
+    console.log(event.target.value);
+    const inputValue = event.target.value
+    setInputData(event.target.value);
+    sendingData.contents[0].parts[0].text=inputValue
+  };
+
   return (
     <div>
-        <div>
-            <p>qns</p>
-            <p>ans</p>
-        </div>
+      <div>
+        <p>qns</p>
+        <p>ans</p>
+      </div>
 
-        <div>
-            <input type="text" placeholder='Ask With Your Ai Friend'/>
-            <button>Submit</button>
-        </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Ask With Your Ai Friend"
+          onChange={handleInput}
+        />
+        <button>Submit</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chatbot
+export default Chatbot;
